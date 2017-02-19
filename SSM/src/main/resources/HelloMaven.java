0f1c1baebda267c1b956185ceb367fa3c64e6206
liaodingyuan <634748078@qq.com>
2017-02-20 02:48:25 +0800

    新工程创建

---
+++ SSM/src/main/resources/HelloMaven.java
@@ -0,0 +1,13 @@
+package main.resources;
+
+/**
+ * Created by liaody on 2017/2/20.
+ */
+public class HelloMaven {
+    public HelloMaven(){
+
+    }
+    public static void main(String[] args){
+        System.out.println("Hello Maven!");
+    }
+}
